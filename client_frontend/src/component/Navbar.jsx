aec840d2f313cd749bfbf4d4fcba0e9df4f3d51e
archikirar <archi@betterself.today>
2024-09-04 00:23:47 +0530

    Added the Links and styled the Navbar

--- client_frontend/src/component/Navbar.jsx
+++ client_frontend/src/component/Navbar.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import styles from "./Navbar.module.css";
+import car_img from "../assets/vehikart-high-resolution-logo-transparent.png"
+import cart_img from "../assets/shopping-cart.png"
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
       <nav className={styles["navbar"]}>
-        <div className={styles["logo"]}>
-          <a href="/">YouNeedCar</a>
+        <div className={styles["navbar-header"]}>
+          <div className={styles["logo"]}>
+            <a href="/"><img src={car_img} alt="logo"/></a>
+          </div>
+          <button
+            className={styles["menu-icon"]}
+            onClick={toggleMenu}
+            aria-label="Toggle Menu"
+          >
+            &#9776;
+          </button>
         </div>
-        <div className={styles["navbar-links"]}>
+        <div
+          className={`${styles["navbar-links"]} ${
+            isMenuOpen ? styles["active"] : ""
+          }`}
+        >
           <a href="/home" className={styles["link"]}>
             Home
           </a>
@@ -19,6 +41,10 @@ function Navbar() {
           </a>
           <a href="/contact" className={styles["link"]}>
             Contact
+          </a>
+          <a href="/cart" className={styles["link"]}>
+          Cart
+          <img src={cart_img} alt="logo"/>
           </a>
         </div>
       </nav>
